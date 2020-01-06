c739d752041a107727d32c92c0124c48425585a8
Tony Erskine <tony@sharptop.co>
2020-01-06 16:36:01 -0500

    removed hard coded dashboard ID, moved some local vars to class properties

--- grails-app/controllers/grails4test/ApplicationController.groovy
+++ grails-app/controllers/grails4test/ApplicationController.groovy
@@ -4,35 +4,37 @@ import software.amazon.awssdk.services.quicksight.QuickSightClient
 import software.amazon.awssdk.services.quicksight.model.*
 
 class ApplicationController {
+    String roleName = "embed-dashboard"
+    String namespace = "default"
+    String awsAccountId
+    String dashboardId
+    QuickSightClient quickSightClient
 
-    def index(String awsAccountId, String email) {
-        if (!awsAccountId || !email) {
-            render "'awsAccountId' and 'email' must be specified in the URL.<br/><br/>" +
-                "Format: http://localhost:8080/?awsAccountId=123456789012&email=john.doe@company.com"
+    def index(String awsAccountId, String email, String dashboardId) {
+        if (!awsAccountId || !email || !dashboardId) {
+            render "'awsAccountId', 'email', and 'dashboardId' must be specified in the URL.<br/><br/>" +
+                "Format: http://localhost:8080/?awsAccountId=123456789012&email=john.doe@company.com&dashboardId=1c1fe111-e2d2-3b30-44ef-a0e111111cde"
             return
         }
-        String url = fetchEmbedUrl(awsAccountId, email)
+
+        this.awsAccountId = awsAccountId
+        this.dashboardId = dashboardId
+        quickSightClient = quickSightClient ?: QuickSightClient.create()
+
+        log.info("Retrieving URL for AWS Account: $awsAccountId and user: $email")
+        String url = fetchEmbedUrl(email)
         render "<a href='$url' target='_blank'>Dashboard</a>"
     }
 
-    private String fetchEmbedUrl(String awsAccountId, String email) {
-        String namespace = "default"
+    private String fetchEmbedUrl(String email) {
+        // look up the user by email address
+        // if we don't find the user register them
+        User user = fetchUser(email) ?: registerUser(email)
 
-        // make sure the user is registered
-        QuickSightClient quickSightClient = QuickSightClient.create()
-        User user = quickSightClient.listUsers(ListUsersRequest.builder()
-            .awsAccountId(awsAccountId)
-            .namespace(namespace)
-            .build()
-        ).userList().find { it.email() == email }
-
-        if (!user) {
-            user = registerUser(quickSightClient, awsAccountId, namespace, email)
-        }
-
+        // get the dashboard URL
         String embedUrl = quickSightClient.getDashboardEmbedUrl(GetDashboardEmbedUrlRequest.builder()
             .awsAccountId(awsAccountId)
-            .dashboardId("c390031c-07e1-4490-ae21-b55d46d6c4c0")
+            .dashboardId(dashboardId)
             .userArn(user.arn)
             .identityType(IdentityType.QUICKSIGHT)
             .sessionLifetimeInMinutes(600L)
@@ -44,10 +46,17 @@ class ApplicationController {
         return embedUrl
     }
 
-    private User registerUser(QuickSightClient quickSightClient, String awsAccountId, String namespace, String email) {
-        String roleName = "cc-embedding_quicksight_dashboard_role"
+    private User fetchUser(String email) {
+        return quickSightClient.listUsers(ListUsersRequest.builder()
+            .awsAccountId(awsAccountId)
+            .namespace(namespace)
+            .build()
+        ).userList().find { it.email() == email }
+    }
+
+    private User registerUser(String email) {
         String roleArn = "arn:aws:iam::$awsAccountId:role/$roleName"
-        quickSightClient.registerUser(RegisterUserRequest.builder()
+        return quickSightClient.registerUser(RegisterUserRequest.builder()
             .awsAccountId(awsAccountId)
             .namespace(namespace)
             .identityType(IdentityType.IAM)
